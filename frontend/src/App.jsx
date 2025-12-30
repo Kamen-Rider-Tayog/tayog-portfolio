7dfd51f3fa2343e0705668b52143ae5076fb2949
tayog <basallo.tayog@gmail.com>
2025-12-31 05:34:37 +0800

    Merry Christmas and Happy Holidays!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,7 @@ import BackToTop from './components/BackToTop';
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-[#F0EEE9] dark:bg-gray-900 transition-colors duration-300 overflow-x-hidden">
-      {/* Snowfall Effect - Optional */}
-      {false && ( // Change to true to enable snowfall
+      {true && ( 
         <Snowfall 
           color="#F7CAC9"  
           snowflakeCount={100}  
